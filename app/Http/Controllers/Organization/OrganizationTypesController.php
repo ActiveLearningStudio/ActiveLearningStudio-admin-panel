92bcb5e4268d7dbe0829ef5573edd68bac477545
Antonio Etayo <antonioetayo@gmail.com>
2020-10-12 12:26:09 -0400

    Adding some comments for good measure

--- app/Http/Controllers/Organization/OrganizationTypesController.php
+++ app/Http/Controllers/Organization/OrganizationTypesController.php
@@ -16,23 +16,43 @@ class OrganizationTypesController extends Controller
     use RequestTrait;
     protected $end_point = '/organization-types';
 
+    /**
+     * View lists existing organization types
+     * @return View
+     * @throws \Throwable
+     */
     public function index()
     {
         $orgTypes = $this->getHTTP($this->end_point);
         return view('organization-types.index', ['orgTypes'=>$orgTypes['data']]);
     }
 
+    /**
+     * View provides interface to create a new organization type
+     * @return View
+     */
     public function create()
     {
         return view('organization-types.create');
     }
 
+    /**
+     * View provides an interface to edit an existing organization type
+     * @param int $id
+     * @return View
+     * @throws \Throwable
+     */
     public function edit($id)
     {
         $orgType = $this->getHTTP($this->end_point.'/'.$id);
         return view('organization-types.create', ['orgType' => $orgType]);
     }
 
+    /**
+     * Saves organization type information from create and edit
+     * @return Redirect
+     * @throws \Throwable
+     */
     public function save(Request $req)
     {
         if($req->filled('type_id')){
@@ -55,11 +75,24 @@ class OrganizationTypesController extends Controller
         return redirect('admin/organization-types');
     }
 
+    /**
+     * Deletes existing organization type
+     * @return Redirect
+     * @throws \Throwable
+     */
     public function delete($id) {
         $result = $this->deleteHTTP($this->end_point.'/'.$id);
         return redirect('admin/organization-types');
     }
 
+    /**
+     * Changes the display order for organization types.
+     * It can move a single type up or down
+     * @param int $type_id The id of the org type to move
+     * @param string $direction Move up or down
+     * @return Redirect
+     * @throws \Throwable
+     */
     public function change_order($type_id, $direction){
         $orgType = $this->getHTTP($this->end_point.'/'.$type_id);
         $result = $this->putHTTP(
