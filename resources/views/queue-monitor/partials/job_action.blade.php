b3158b6dce123c5aada500427fb066026e00c7c1
Ahmad Mukhtar <ahmad.curriki@gmail.com>
2020-10-28 12:50:17 +0500

    Retry spelling correction

--- resources/views/queue-monitor/partials/job_action.blade.php
+++ resources/views/queue-monitor/partials/job_action.blade.php
@@ -4,7 +4,7 @@
     <button type="button" class="btn btn-warning dropdown-toggle dropdown-icon" data-toggle="dropdown" aria-expanded="false">
         <span class="sr-only">Toggle Dropdown</span>
         <div class="dropdown-menu" role="menu" x-placement="bottom-start">
-            <a class="dropdown-item" data-id="{{ $job['id'] }}" onclick="retry_job(this)">Retry1</a>
+            <a class="dropdown-item" data-id="{{ $job['id'] }}" onclick="retry_job(this)">Retry</a>
             <a class="dropdown-item" data-id="{{ $job['id'] }}" onclick="forget_job(this)">Forget</a>
         </div>
     </button>
