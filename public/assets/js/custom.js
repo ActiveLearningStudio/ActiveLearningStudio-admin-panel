820a411afce5314a82c10475fa57aa4d7f80c901
Ahmad Mukhtar <68377680+ahmad-curriki@users.noreply.github.com>
2020-11-02 18:45:51 +0500

    [CUR-667] User created at date filter and sorting (#47)
    * User created at date filter and sorting

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -223,6 +223,26 @@ function initializeSelect2(target, url, textProp = ['title'], idProp = 'id') {
 }
 
 /**
+ * Initialize the date range picker
+ * @param target
+ */
+function initializeDateRange(target) {
+    $(target).daterangepicker({
+        startDate: moment().subtract(29, 'days'),
+        endDate: moment(),
+        showDropdowns: true,
+        ranges: {
+            'Today': [moment(), moment()],
+            'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+            'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+            'This Month': [moment().startOf('month'), moment().endOf('month')],
+            'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        }
+    }, cb);
+}
+
+/**
  * image preview
  * @param input
  */
